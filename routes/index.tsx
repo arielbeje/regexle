e6f5c09b75558bec70b8ce4db69adb46d827a27f
arielbeje <arielbeje@protonmail.com>
2022-08-03 22:33:57 +0300

    index: Add game explanation

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,7 +6,20 @@ import RegexleGame from "../islands/RegexleGame.tsx";
 export default function Home() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <p>Welcome to Regexle</p>
+      <p>
+        Welcome to Regexle. A random RegEx pattern has been chosen, and you must
+        guess it. You can attempt to guess as many times as you want.
+      </p>
+      <p>
+        You can enter sentences to see if they match, and what matches inside
+        them.
+      </p>
+
+      <p>
+        Note: The pattern is case insensitive, and matches the following regex:
+        {" "}
+        <pre>[.*a-z0-9]+</pre>
+      </p>
       <RegexleGame />
     </div>
   );
