facf4fa929007b3994ffb092f549a2f55712c77d
arielbeje <arielbeje@protonmail.com>
2022-08-05 16:10:01 +0300

    consts: Add more patterns, shuffling existing ones

--- utils/consts.ts
+++ utils/consts.ts
@@ -1,5 +1,17 @@
 export const PATTERN_FLAGS = "gi";
-export const PATTERNS = ["abc", "a", ".*a.*", ".*i.*", ".*ab", ".*c", ".*i"];
+export const PATTERNS = [
+  "a",
+  ".*i",
+  ".*c",
+  "12.*",
+  ".*ab",
+  ".*a.*",
+  "1..4",
+  ".va",
+  ".*i.*",
+  "abc",
+  ".*l",
+];
 
-export const FIRST_PATTERN_DAY = 19208
-export const STORAGE_LAST_SOLVED_DAY = "regexle_last_solved_day"
+export const FIRST_PATTERN_DAY = 19208;
+export const STORAGE_LAST_SOLVED_DAY = "regexle_last_solved_day";
